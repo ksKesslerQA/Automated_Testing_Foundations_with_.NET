84c6992fcd025fda0e77b63c302f51cea31adc66
Karyna Shevchenko <shevchenkoks1810@gmail.com>
2021-12-19 00:44:51 +0200

    Added Program.cs file

---
+++ DevelopmentAndBuildToolsTask1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DevelopmentAndBuildToolsTask1
+{
+    class Program
+    {
+        /* 
+        Практические задания
+
+Написать программу, которая принимает из командной строки последовательность символов (строку)
+        в качестве аргумента и выводит в консоль максимальное количество неодинаковых 
+        последовательных символов в строке
+        */
+
+        static void Main(string[] args)
+        {
+            string EnteredText = Console.ReadLine();
+            char[] ArrayText = EnteredText.ToCharArray();
+            var maxNumberUnequalChar = 1;
+
+            for (int i = 0; i < ArrayText.Length-1; i++)
+            {
+                if (ArrayText[i] != ArrayText[i+1])
+                {
+                   maxNumberUnequalChar += 1;
+                }
+
+            }
+            Console.WriteLine("Maximum number of unequal consecutive characters: " + maxNumberUnequalChar);
+
+        }
+    }
+}
