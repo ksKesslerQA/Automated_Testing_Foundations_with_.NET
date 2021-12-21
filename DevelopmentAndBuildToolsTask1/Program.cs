c125512cbc676687504789955a57e24b73291d1a
Karyna Shevchenko <shevchenkoks1810@gmail.com>
2021-12-22 01:46:16 +0200

    Fix for DevelopmentAndBuildToolsTask1

--- DevelopmentAndBuildToolsTask1/Program.cs
+++ DevelopmentAndBuildToolsTask1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DevelopmentAndBuildToolsTask1
 {
@@ -7,25 +8,44 @@ namespace DevelopmentAndBuildToolsTask1
         /* 
         Практические задания
 
-Написать программу, которая принимает из командной строки последовательность символов (строку)
+        Написать программу, которая принимает из командной строки последовательность символов (строку)
         в качестве аргумента и выводит в консоль максимальное количество неодинаковых 
-        последовательных символов в строке
+        последовательных символов в строке.
+
+        Например,
+        "123441" > 4
+        "ksqaaksqksq" > 7
+        "kkkk" > 1
+        "sskesler" > 7
+
         */
 
         static void Main(string[] args)
         {
             string EnteredText = Console.ReadLine();
             char[] ArrayText = EnteredText.ToCharArray();
-            var maxNumberUnequalChar = 1;
+
+            var unequalNumberList = new List <int> ();
+            var k = 0;
+            unequalNumberList.Add(1);
+            
 
             for (int i = 0; i < ArrayText.Length-1; i++)
             {
-                if (ArrayText[i] != ArrayText[i+1])
+                if (ArrayText[i] == ArrayText[i+1])
                 {
-                   maxNumberUnequalChar += 1;
+                    unequalNumberList.Add(1);
+                    k += 1;
+                    continue;
                 }
+                unequalNumberList[k] += 1;
+                
 
             }
+            unequalNumberList.Sort();
+            unequalNumberList.Reverse();
+            var maxNumberUnequalChar = unequalNumberList[0];
+
             Console.WriteLine("Maximum number of unequal consecutive characters: " + maxNumberUnequalChar);
 
         }
